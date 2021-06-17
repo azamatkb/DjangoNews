5c2839d76255d3d0985af707839de6161914ec43
tenthent <kayratsagynbekov@gmail.com>
2021-06-17 20:19:30 +0600

    init2

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+
 from .views import *
 
-
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
 ]
