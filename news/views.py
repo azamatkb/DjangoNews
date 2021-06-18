1f2e691f9832014a91ae0952325d020e3eb3b8dc
Azamat <azamataudi@mail.ru>
2021-06-18 18:19:38 +0600

    first commit

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django import forms
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
+from .forms import NewsForm
 
 
 def index(request):
@@ -22,3 +24,13 @@ def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect('/')
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
